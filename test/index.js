8e96dbe95b4e47c8eaf7bfc3d2b893f7a64c3f20
Titus Wormer <tituswormer@gmail.com>
2023-07-18 17:16:50 +0200

    Change to use `exports`

--- test/index.js
+++ test/index.js
@@ -7,11 +7,11 @@ import fs from 'node:fs/promises'
 import process from 'node:process'
 import test from 'node:test'
 import {isHidden} from 'is-hidden'
-import {fromXml} from '../index.js'
+import {fromXml} from 'xast-util-from-xml'
 
 test('fromXml', async function (t) {
   await t.test('should expose the public api', async function () {
-    assert.deepEqual(Object.keys(await import('../index.js')).sort(), [
+    assert.deepEqual(Object.keys(await import('xast-util-from-xml')).sort(), [
       'fromXml'
     ])
   })
